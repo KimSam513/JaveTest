d4d23590e8bf97a919c55d923f50bac0b14b93e6
user <user@10.85.2.99>
2022-09-07 15:36:20 +0900

    account 예제

---
+++ src/unit02/account/Account.java
@@ -0,0 +1,57 @@
+package unit02.account;
+
+public class Account {
+	
+	private String name;
+	private int no;
+	private int balance;
+	
+	public Account() {}
+	public Account(String name, int no) {
+		this.name = name;
+		this.no = no;		
+	}
+	public Account(String name, int no, int balance) {
+		this.name = name;
+		this.no = no;
+		this.balance = balance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(int money) {
+		balance += money;
+		System.out.println(name + "´Ô" + money + "¿ø ÀÔ±Ý");
+	}
+	
+	public void withdraw(int money) {
+		if(balance < money)
+			System.out.println(name + "´Ô" + money + "¿ø Ãâ±Ý ½ÇÆÐ-ÇöÀç¤Ó ÀÜ¾× " + balance + "¿ø");
+		else {
+			balance -= money;
+			System.out.println(name + "´Ô" + money + "¿ø Ãâ±Ý");
+		}
+	}
+	
+	public void printInfo() {
+		System.out.println("[°èÁÂÁ¤º¸] ¿¹±ÝÁÖ: " + name + "°èÁÂ¹øÈ£ : " + no + "ÇöÀç ÀÜ°í : " + balance);
+	}
+	
+
+}
---
+++ src/unit02/account/AccountMain.java
@@ -0,0 +1,24 @@
+package unit02.account;
+
+public class AccountMain {
+
+	public static void main(String[] args) {	
+		Account a1 = new Account("±èÅÂÈñ", 101);
+		a1.setBalance(8000);
+		
+		Account a2 = new Account("¹è¼öÁö", 102, 0);
+		
+		a1.printInfo();
+		a2.printInfo();
+		
+		a1.withdraw(5000);
+		a2.deposit(100000);
+		a1.deposit(1000);
+		a2.withdraw(7000);
+		
+		a1.printInfo();
+		a2.printInfo();
+		
+		a1.withdraw(5000);
+	}
+}
